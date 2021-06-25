f7538ec50072e6f3ba3040f6fd33c8b03444de0e
Maciej Para <m.para@nogravity.io>
2021-06-25 15:24:50 +0200

    Updates

--- static/js/11.7bbc1eab.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[11],{95:function(p,r,t){"use strict";t.r(r);t(2);var e=t(8);r.default=function(){return Object(e.jsx)("h1",{children:"error"})}}}]);
-//# sourceMappingURL=11.7bbc1eab.chunk.js.map
---
+++ static/js/13.a513b7de.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[13],{97:function(p,r,t){"use strict";t.r(r);t(2);var e=t(8);r.default=function(){return Object(e.jsx)("h1",{children:"error"})}}}]);
+//# sourceMappingURL=13.a513b7de.chunk.js.map
